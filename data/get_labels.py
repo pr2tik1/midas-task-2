2a25b9444a791a70e29d6e8faf4ca6882ffaf00b
pr2tik1 <pr2tik1@gmail.com>
2021-04-11 12:40:58 +0530

    zipped out data

--- data/get_labels.py
+++ data/get_labels.py
@@ -2,6 +2,12 @@
 Script to get filenames and store it in a csv file. 
 Initially it is required to create csv file named test_labels.csv. 
 Process the data gathered into labels(targets) of images. 
+
+NOTE : Along with this Excel and other preprocessing steps within the notebooks were taken. So running this script will not reproduce the labels.csv file.
+
+The steps were: 
+> Initially a blank csv file is created. Then the file names in dataset folder is read and preprocessed as class. Finally stored in the blank csv, then loaded in notebooks for dataset class and data loaders. 
+
 """
 
 import os, csv
